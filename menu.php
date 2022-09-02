5663bba90ff492d293db193da8724064c7ec9b87
Pio-cmd <112107512+Pio-cmd@users.noreply.github.com>
2022-09-02 15:26:42 +0200

    a big menu mess

--- menu.php
+++ menu.php
@@ -7,36 +7,75 @@ if (isset($_SESSION['uid']))
     $query = "SELECT * FROM `user` WHERE `id` = '$uid'";
     $run = mysqli_query($conn, $query);
     $data = mysqli_fetch_assoc($run);
+	$id = mysqli
 }
 else{
 
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>FoodHub</title>
-	<!-- font awesome -->
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
-  	<!-- Bootstrap -->
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/index.css">
-	<!-- Google font -->
-	<link href="https://fonts.googleapis.com/css2?family=Knewave&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="images/favicon.png" type="">
+
+  <title>FoodHub</title>
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <!-- nice select  -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css" integrity="sha512-CruCP+TD3yXzlvvijET8wV5WxxEh5H8P4cmz0RFbKK6FlZ2sYl3AEsKlLPHbniXKSrDdFewhbmBK5skbdsASbQ==" crossorigin="anonymous" />
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+
 </head>
 
 <body>
-<div class="content-wrapper">
-	<div id="menu" class="site-title">
-		<div class="hub">
-		<span>The</span><span>menu</span>
-</div>
-</div>
+	<script>
+$(document).ready(function() {
 
-</div>
+<?php
 
-</body
+	$sql = "SELECT * FROM `menu` WHERE `id` = $id";
+	$retval = mysql_query( $sql, $conn );
+	if(! $retval )
+	{
+	  die('Could not get data: ' . mysql_error());
+	}
+	while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
+	{
+	?>
+		$('<div/>', {
+				'id': 'div _<?php echo $row["id"]; ?>',
+				'class': 'some_class_name',
+				'html': 'i am div with id=div '+ 
+						'<?php echo $row["id"]; ?>'+  
+						' <?php echo $row["name"]; ?>'+
+						'<img src="PATH_TO_IMAGE/<?php echo $row["image_name"]; ?>" /> '
+					  
+			}).appendTo('.body1');
+
+<?php   } ?>
+
+});
+</script>
+</body>
+</html>
